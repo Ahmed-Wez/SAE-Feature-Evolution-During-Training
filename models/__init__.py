2c3ebb45b248c8c28563051bea12aa680ec549e7
Ahmed Wez <ahmed.moh.wez@gmail.com>
2025-12-24 14:27:48 +0200

    Update __init__.py

--- models/__init__.py
+++ models/__init__.py
@@ -1,6 +1,3 @@
-"""
-Model loading utilities for Pythia checkpoints
-"""
 from .load_pythia import PythiaCheckpointLoader, get_available_checkpoints
 
 __all__ = ['PythiaCheckpointLoader', 'get_available_checkpoints']
